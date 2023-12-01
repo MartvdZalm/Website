f7d7942c8851792a213a4d375897f77ff6a214dc
MartvdZalm <martvanderzalm@gmail.com>
2023-12-01 10:05:37 +0100

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <component v-bind:is="component"></component>
+</template>
+
+<script>
+import Boot from '@/components/Boot.vue';
+
+export default {
+    components: {
+        'boot': Boot,
+    },
+    data () {
+        return {
+            component: "boot",
+        }
+    }
+}
+</script>
