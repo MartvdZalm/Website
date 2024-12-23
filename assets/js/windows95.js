6a2596ad49a890498551831ca9007a3062ab5436
MartvdZalm <martvanderzalm@gmail.com>
2024-12-23 23:52:06 +0100

    commit

--- assets/js/windows95.js
+++ assets/js/windows95.js
@@ -1,10 +1,21 @@
 const start_menu_btn = document.querySelector(".taskbar__btn--start")
 const start_menu = document.querySelector(".taskbar__menu")
 
+const programsItem = document.querySelector(".programs");
+const programsMenu = document.querySelector(".sub-programs");
+
+
 start_menu_btn.addEventListener("click", function() {
     this.classList.toggle("checked")
     start_menu.classList.toggle("taskbar__menu--open")
 })
+
+
+programsItem.addEventListener("click", function() {
+    console.log(programsMenu);
+    programsMenu.classList.toggle("menu-open");
+    console.log(programsMenu);
+});
 
 
 function updateDateTime()
